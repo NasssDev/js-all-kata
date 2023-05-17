6e1876f381582bd8a7609e841cb265f25da7a819
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-05-17 09:57:35 +0200

    win from top to bottom

--- src/index.ts
+++ src/index.ts
@@ -19,16 +19,28 @@ export type Game = {
   nextPlayer: Player;
 };
 
-export const computeNextGame = (game: Game, action : Action): Game => {
+export const computeNextGame = (game: Game, action : Action): Game | string => {
   action.player === "O" ? game.grid[action.coordinates.y][action.coordinates.x] = "O" : game.grid[action.coordinates.y][action.coordinates.x] = "X";
-  action.player === "O" ? game.nextPlayer = "X" : game.nextPlayer = "O"
-    return {
-      grid: game.grid,
-      nextPlayer: game.nextPlayer,
-    };
+  action.player === "O" ? game.nextPlayer = "X" : game.nextPlayer = "O";
+  if (checkIfWinFromLeftToRight(game.grid, action.player)){
+    return "winner "+action.player;
+  }
+  return {
+    grid: game.grid,
+    nextPlayer: game.nextPlayer,
+  };
 };
 
-export const checkIfWinFromLeftToRight = (grid : Grid, currentPlayer : Player) : string => {
+export const checkIfWinFromLeftToRight = (grid : Grid, currentPlayer : Player) : boolean => {
+  let winFromTopToBottom
   let winFromLeftToRight = grid[0].every(cell => cell === currentPlayer) || grid[1].every(cell => cell === currentPlayer) || grid[2].every(cell => cell === currentPlayer)
-  return winFromLeftToRight ? "winner "+currentPlayer : "no win yet"
+  for (let i =0;i < 3;i++) {
+    winFromTopToBottom = grid.every((row) => row[i] === currentPlayer);
+    if (winFromTopToBottom) break;
+  }
+  return winFromLeftToRight || winFromTopToBottom;
+}
+
+export const checkIfWinFromTopToBottom = (grid : Grid, currentPlayer : Player) : string => {
+
 }
